2369a4f772dfecf6387b56f49f98795e7659928c
Your Name <ruhulmus@gmail.com>
2022-02-06 14:24:34 +0600

    arrya list exmaple

---
+++ src/ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> number = new ArrayList<>(); //intializatiton and empty Array list
+        number.add(10);
+        number.add(15);
+        number.add(18);
+        number.add(3,20); //index 3(defined) and element 20
+
+        System.out.println("Array List Size : " + number.size());
+        System.out.println("Array List: " + number);
+    }
+}
