de4c367525582255e45b8d72dc8a2e161035582e
Your Name <ruhulmus@gmail.com>
2022-02-12 23:27:38 +0600

    wrapper class updated

--- src/WrapperClass.java
+++ src/WrapperClass.java
@@ -7,9 +7,12 @@ public class WrapperClass {
         System.out.println("y = "+ y);
 
         //Unboxing : Object -> Primitive
-
         Double a = new Double(10.5);
         double b = a;
         System.out.println("b = "+ b);
+
+        Double c =  new Double(15.5);
+        double d = c.doubleValue();
+        System.out.println("d =" + d);
     }
 }
