fb6a468161275d0624f0e0b79e2ef5db2f4d44f7
Your Name <ruhulmus@gmail.com>
2022-02-12 23:15:14 +0600

    aboxing

---
+++ src/WrapperClass.java
@@ -0,0 +1,11 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+
+        // Autoboxing : Primitive -> Object
+        int x = 10;
+        Integer y = Integer.valueOf(x);
+
+        System.out.println("y = "+ y);
+
+    }
+}
