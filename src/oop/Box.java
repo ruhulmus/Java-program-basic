66f3d66fc4e18748b385d237b89f71864c98327a
Your Name <ruhulmus@gmail.com>
2022-02-15 22:18:18 +0600

    display 2 box values

--- src/oop/Box.java
+++ src/oop/Box.java
@@ -1,5 +1,19 @@
 package oop;
 
 public class Box {
+    double height;
+    double width;
+    double depth;
 
+    Box(double h, double w, double d){
+        height = h;
+        width = w;
+        depth = d;
+    }
+
+    void dislayVol(){
+        System.out.println("Height : " + height);
+        System.out.println("Width : " + width);
+        System.out.println("Depth : " + depth);
+    }
 }
--- src/oop/BoxVolume.java
+++ src/oop/BoxVolume.java
@@ -2,6 +2,12 @@ package oop;
 
 public class BoxVolume {
     public static void main(String[] args) {
+        Box box1 = new Box(10,10,10);
+        Box box2 = new Box(20,30,10);
 
+        System.out.println("Box 1 :");
+        box1.dislayVol();
+        System.out.println("Box 2 :");
+        box2.dislayVol();
     }
 }
