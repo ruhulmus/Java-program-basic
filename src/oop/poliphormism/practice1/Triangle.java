ba0f981e8341209ed48a3f0a9e3a2045c80a5e8d
Your Name <ruhulmus@gmail.com>
2022-02-23 01:52:52 +0600

    poliphorsim practice 1

--- src/oop/poliphormism/practice1/Triangle.java
+++ src/oop/poliphormism/practice1/Triangle.java
@@ -1,4 +1,14 @@
 package oop.poliphormism.practice1;
 
-public class Triangle {
+public class Triangle extends Shape{
+    double base,length;
+
+    Triangle(double base, double length){
+        this.base = base;
+        this.length = length;
+    }
+
+    double area(double base, double length){
+        return .5*base*length;
+    }
 }
