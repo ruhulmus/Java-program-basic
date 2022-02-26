7e551b88db83cbf87797517090dec4c892da13bb
ruhulmus <ruhulmus@gmail.com>
2022-02-26 22:56:17 +0600

    abstracton practice

--- src/oop/abstraction/Main.java
+++ src/oop/abstraction/Main.java
@@ -3,6 +3,11 @@ package oop.abstraction;
 public class Main {
     public static void main(String[] args) {
         Shape shape ;
+        shape = new Circle(20);
+        shape.area();
+
+        shape = new Rectangle(10,15);
+        shape.area();
 
     }
 }
