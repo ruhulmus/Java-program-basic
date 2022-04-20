d9855d20de30639c1b0d8c64b4255e5beb8b7037
ruhulmus <ruhulmus@gmail.com>
2022-04-20 14:03:31 +0600

    update foreach

--- src/ForEach.java
+++ src/ForEach.java
@@ -4,10 +4,13 @@ public class ForEach {
         int numbers [] = {5,6,2,4};
 
         int i=0;
+        int sum = 0;
         //Fo reach
         for (int number : numbers){
             System.out.println("Number"+i+" "+ number);
+            sum += number;
             i++;
         }
+        System.out.println("Total" + sum);
     }
 }
