eac2f1f5465d6e469370060b3314154971f4d537
ruhulmus <ruhulmus@gmail.com>
2022-04-20 13:57:57 +0600

    loop update

--- src/Loop.java
+++ src/Loop.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Loop {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number : ");
+        // Program to find the sum of natural numbers from 1 to 5.
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter number : ");
 
         int n = 5;
         int sum = 0;
 
-        for (int i=n; i>=1; --i){
+        for (int i=1; i<=n; i++){
             sum += i;
         }
+//        for (int i=n; i>=1; --i){
+//            sum += i;
+//        }
         System.out.println("Total " + sum);
     }
 }
