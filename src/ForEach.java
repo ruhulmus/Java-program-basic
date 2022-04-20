bfd33f32938fd80066d102ef1f609b747c85e037
ruhulmus <ruhulmus@gmail.com>
2022-04-20 14:01:03 +0600

    loop update

---
+++ src/ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+        // Create array
+        int numbers [] = {5,6,2,4};
+
+        //Fo reach
+        for (int number : numbers){
+            System.out.println("Number "+ number);
+        }
+    }
+}
