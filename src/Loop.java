1fe3c172821bad9221afff7f156b205789b5d062
ruhulmus <ruhulmus@gmail.com>
2022-04-20 13:42:44 +0600

    loop update

--- src/Loop.java
+++ src/Loop.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Loop {
     public static void main(String[] args) {
-        System.out.println("this is test");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number : ");
+
+        int n = 5;
+        int sum = 0;
+
+        for (int i=0; i<=n; i++){
+            sum += i;
+        }
+        System.out.println("Total " + sum);
     }
 }
