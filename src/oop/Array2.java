a2babdeb47d0f91c47a594a95f534e5d5f96f75d
ruhulmus <ruhulmus@gmail.com>
2022-06-26 11:59:11 +0600

    array exmaple

--- src/oop/Array2.java
+++ src/oop/Array2.java
@@ -10,5 +10,25 @@ public class Array2 {
         System.out.println("Original numeric array :" + Arrays.toString(myInt));
         Arrays.sort(myInt);
         System.out.println("Sorted Array: " + Arrays.toString(myInt) );
+
+
+        int[] arr = new int[] { 6, 8, 7, 4, 312, 78, 54, 9, 12, 100, 89, 74 };
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("i : "+ i);
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("j : "+ j);
+                int tmp = 0;
+                if (arr[i] > arr[j]) {
+                    System.out.println("arr[i] : "+ arr[i]);
+                    System.out.println("arr[j] : "+ arr[j]);
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
     }
 }
