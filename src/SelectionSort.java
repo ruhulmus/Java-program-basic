3d7283f8c9468c8d9bc98d8e215df54932d03bed
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-15 01:33:41 +0600

    selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,2 +1,16 @@
 public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {2,4,10,5,32,45};
+
+        int i, j,min_idx, temp,n;
+        n = arr.length;
+        for (i = 0; i<n-1;i++){
+            min_idx = i;
+
+            for (j=i+1; j< n ; j++){
+
+            }
+        }
+
+    }
 }
