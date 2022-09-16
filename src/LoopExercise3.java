5076a3af4be615e2292bdb85290cf818aa96bfdf
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-17 01:43:10 +0600

    Loop exercise 3

--- src/LoopExercise3.java
+++ src/LoopExercise3.java
@@ -6,18 +6,26 @@ public class LoopExercise3 {
         Scanner input = new Scanner(System.in);
         System.out.print("Please Enter a number : ");
         int num = input.nextInt();
-        int i, sum=0,temp;
+        int i, sum = 0,temp;
+//
+//        for(i= 1;i<=num;i++){
+////            System.out.print(" "+ i);
+//            if (num%i == 0){
+//                if (i < num){
+//                    sum += i;
+//                    System.out.print(" "+ i);
+//                }
+//            }
+//        }
+//        System.out.print(" = "+ sum);
 
-        for(i= 1;i<=num;i++){
-//            System.out.print(" "+ i);
-            if (num%i == 0){
-
-                if (i < num){
-                    sum += i;
-                    System.out.print(" "+ i);
+            for (i=1;i<=num/2;i++){
+                if (num%i == 0){
+                    sum +=i;
+                    System.out.print(" "+i);
                 }
             }
-        }
-        System.out.print(" = "+ sum);
+            System.out.print(" \n" + sum);
+
     }
 }
