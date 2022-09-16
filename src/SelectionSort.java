ab4ad5d3f144abf7e199fbdc29b4c48216cf5e07
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-16 21:21:03 +0600

    loop erxercise add

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -5,6 +5,7 @@ public class SelectionSort {
         int arr[] = {2,4,-10,5,32,45};
 
         int i, j,min_idx, temp,n;
+
         n = arr.length;
         for (i = 0; i<n-1;i++){
             min_idx = i;
