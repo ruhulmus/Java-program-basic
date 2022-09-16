7977b4aa27aa50019426d15fcc6547293c031b3a
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-17 00:15:04 +0600

    Loop exercize

--- src/LoopExercize.java
+++ src/LoopExercize.java
@@ -3,11 +3,21 @@ import java.util.Scanner;
 public class LoopExercize {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please Enter your number1 : ");
+        System.out.print("Please Enter your number : ");
         int num = input.nextInt();
+        int i;
 
         System.out.println("numbers : ");
-        for (int i=2; i <= num ; i+=2){
+//        for (i=1; i <= num ; i++){
+//            System.out.print(" "+ i);
+//        }
+        //Output 2 4 6 8 10 --- n
+//        for (i=2; i <= num ; i+=2){
+//            System.out.print(" "+ i);
+//        }
+
+       //Output  100 99 98 97 96 ... 2
+        for ( i = num; i > 1; i--){
             System.out.print(" "+ i);
         }
 
