12608599157440896505fceb59eeee019fe6b1a0
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-16 21:53:06 +0600

    0-9 poriunt

--- src/ArraySwipe.java
+++ src/ArraySwipe.java
@@ -2,18 +2,18 @@ import java.util.ArrayList;
 
 public class ArraySwipe {
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        arr.add(10);
-        arr.add(24);
-        arr.add(22);
-        arr.add(15);
-
-        System.out.println("Array :" + arr);
-
-        int a = arr[3];
-        arr[3]= arr[1];
-        arr[1]= a;
+//        ArrayList<Integer> arr = new ArrayList<>();
+//
+//        arr.add(10);
+//        arr.add(24);
+//        arr.add(22);
+//        arr.add(15);
+//
+//        System.out.println("Array :" + arr);
+//
+//        int a = arr[3];
+//        arr[3]= arr[1];
+//        arr[1]= a;
 
     }
 }
--- src/LoopExercize.java
+++ src/LoopExercize.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class LoopExercize {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Enter your number1 : ");
+        int num = input.nextInt();
+
+        System.out.println("numbers : ");
+        for (int i=0; i < num ; i++){
+            System.out.print(" "+ i);
+        }
+
+    }
 }
