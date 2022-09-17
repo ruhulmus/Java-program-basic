56d3eaeb88af5448f793c3c76af0a85cad302038
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-17 14:13:21 +0600

    Loop exercise 3

--- src/LoopExercise4.java
+++ src/LoopExercise4.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class LoopExercise4 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter a Number : ");
+        int num = input.nextInt();
+        int count =0;
+        for (int i=2; i<num;i++){
+            if (num%i == 0){
+                count ++;
+                break;
+            }
+
+        }
+        if (count == 0){
+            System.out.print("Prime number");
+        }
+        else{
+            System.out.print("Not a prime number");
+        }
+    }
 }
