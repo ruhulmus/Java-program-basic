f9840b7832caeb45c9f9984f51bd5da30e4099cf
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-17 17:10:05 +0600

    prime numbe example

--- src/PrimeNumber3.java
+++ src/PrimeNumber3.java
@@ -1,32 +1,59 @@
 import java.util.Scanner;
 
 public class PrimeNumber3 {
-    static void checkPrime(int num){
+//    static void checkPrime(int num){
+//
+//        int i, count=0;
+//        if (num == 0 | num == 1){
+//            System.out.print("Not a Prime");
+//        }else{
+//            for (i=2; i<num;i++){
+//                if(num%i == 0){
+//                    count ++;
+//                    break;
+//                }
+//            }
+//            if (count == 0){
+//                System.out.print("Is a Prime");
+//            }
+//            else{
+//                System.out.print("Not a Prime");
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Please Enter a Number : ");
+//
+//        int num = input.nextInt();
+//        checkPrime(num);
+//    }
 
-        int i, count=0;
-        if (num == 0 | num == 1){
-            System.out.print("Not a Prime");
-        }else{
-            for (i=2; i<num;i++){
-                if(num%i == 0){
-                    count ++;
-                    break;
-                }
-            }
-            if (count == 0){
-                System.out.print("Is a Prime");
-            }
-            else{
-                System.out.print("Not a Prime");
-            }
+    static void getPrime(int start, int end){
+        int i;
+        System.out.print("Prime numbers Between " + start +" and "+ end + " : ");
+        for (i=start;i<end;i++){
+            if (isPrime(i))
+                System.out.print(" " +i);
         }
+    }
+
+    static boolean isPrime(int n){
+        if (n%2 == 0)
+            return false;
+        else
+            return true;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please Enter a Number : ");
+        System.out.print("Enter your start number : ");
+        int start = input.nextInt();
 
-        int num = input.nextInt();
-        checkPrime(num);
+        System.out.print("Enter your End number : ");
+        int end = input.nextInt();
+
+        getPrime(start, end);
     }
 }
