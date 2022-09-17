b573693fbc4c74f8cb3c80a426870d6cd5cbf317
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-17 17:18:42 +0600

    prime numbe example

--- src/PrimeNumber3.java
+++ src/PrimeNumber3.java
@@ -8,7 +8,7 @@ public class PrimeNumber3 {
 //            System.out.print("Not a Prime");
 //        }else{
 //            for (i=2; i<num;i++){
-//                if(num%i == 0){
+//                if(num%2 == 0){
 //                    count ++;
 //                    break;
 //                }
@@ -30,30 +30,59 @@ public class PrimeNumber3 {
 //        checkPrime(num);
 //    }
 
-    static void getPrime(int start, int end){
-        int i;
-        System.out.print("Prime numbers Between " + start +" and "+ end + " : ");
-        for (i=start;i<end;i++){
-            if (isPrime(i))
-                System.out.print(" " +i);
+
+//Display prime number bwetween range
+
+//    static void getPrime(int start, int end){
+//        int i;
+//        System.out.print("Prime numbers Between " + start +" and "+ end + " : ");
+//        for (i=start;i<end;i++){
+//            if (isPrime(i))
+//                System.out.print(" " +i);
+//        }
+//    }
+//
+//    static boolean isPrime(int n){
+//        if (n%2 == 0)
+//            return false;
+//        else
+//            return true;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter your start number : ");
+//        int start = input.nextInt();
+//
+//        System.out.print("Enter your End number : ");
+//        int end = input.nextInt();
+//
+//        getPrime(start, end);
+//    }
+
+    static void checkPrime(int n){
+        int i, count=0;
+        if(n==0 | n== 1){
+            System.out.print("Not a Prime");
         }
-    }
+        else{
+            for(i=2; i <n; i++){
+                if (n%i == 0){
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0){
+                System.out.print("Is a Prime");
+            }else
+                System.out.println("Not a Prime");
+        }
 
-    static boolean isPrime(int n){
-        if (n%2 == 0)
-            return false;
-        else
-            return true;
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your start number : ");
-        int start = input.nextInt();
-
-        System.out.print("Enter your End number : ");
-        int end = input.nextInt();
-
-        getPrime(start, end);
-    }
+        System.out.print("Please enter a number : ");
+        int num = input.nextInt();
+        checkPrime(num);
+     }
 }
