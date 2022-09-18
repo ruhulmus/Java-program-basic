43e6b5eaaf058109866f6c6720c38309459bc546
ruhul.shadhin <ruhul.amin@shadhinlab.com>
2022-09-19 01:10:17 +0600

    loop

--- src/LoopExercise7.java
+++ src/LoopExercise7.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class LoopExercise7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number : ");
+
+        int num = input.nextInt();
+
+        int sum = 0;
+        while(true){
+            sum += num%10;
+            num /=10;
+
+            if (num == 0){
+                break;
+            }
+        }
+        System.out.println(sum );
+    }
 }
