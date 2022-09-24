527c1f44888de294b05538d890133e97a50603d2
ruhulmus <ruhulmus@gmail.com>
2022-09-24 23:48:40 +0600

    string reverse

---
+++ src/LoopExercise8.java
@@ -0,0 +1,8 @@
+public class LoopExercise8 {
+    public static void main(String[] args) {
+        String str = "test text";
+
+        for (int i= str.length() ; i>=0 ; i--)
+            System.out.print(str.charAt(i));
+    }
+}
