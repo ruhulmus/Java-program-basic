bc8db2aa66c90cf45f44469837820ff92c50591e
ruhulmus <ruhulmus@gmail.com>
2022-09-24 23:49:31 +0600

    string reverse

--- src/LoopExercise8.java
+++ src/LoopExercise8.java
@@ -2,7 +2,7 @@ public class LoopExercise8 {
     public static void main(String[] args) {
         String str = "test text";
 
-        for (int i= str.length() ; i>=0 ; i--)
+        for (int i= str.length()-1; i>=0 ; i--)
             System.out.print(str.charAt(i));
     }
 }
