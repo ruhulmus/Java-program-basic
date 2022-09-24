1ad01b67505967efeeeebf852af2888991c12b16
ruhulmus <ruhulmus@gmail.com>
2022-09-25 01:12:06 +0600

    string reverse

--- src/LoopExercise8.java
+++ src/LoopExercise8.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class LoopExercise8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter a text : ");
 
         String str = input.nextLine();
         for (int i= str.length()-1; i>=0 ; i--)
