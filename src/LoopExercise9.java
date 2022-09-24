93a3e65612ba3ba63a7b1526c5a1473b783cdbb7
ruhulmus <ruhulmus@gmail.com>
2022-09-25 01:34:42 +0600

    palindrom

--- src/LoopExercise9.java
+++ src/LoopExercise9.java
@@ -1,2 +1,16 @@
 public class LoopExercise9 {
+    public static void main(String[] args) {
+        String str = "teet";
+
+        int i , j = str.length()-1;
+        boolean isPalindrop = true;
+        for (i=0;i<j;i++){
+            if (str.charAt(i)==str.charAt(j))
+                continue;
+            isPalindrop = false;
+            break;
+            j--;
+        }
+        System.out.print(isPalindrop?"palindrom":"not palindrom");
+    }
 }
