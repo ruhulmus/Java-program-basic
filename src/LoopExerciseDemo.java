3d99bbb0c866e9da689c2d2f4a96d5c7e13151cf
ruhulmus <ruhulmus@gmail.com>
2022-09-30 02:01:27 +0600

    loop Exercise demo

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -6,12 +6,23 @@ public class LoopExerciseDemo {
         System.out.print("Please enter a number : ");
         int num = input.nextInt();
 
-        int i, sum = 0;
-
-        for(i = 1; i<= num/2;i++){
-            if(num%i == 0)
-                sum+=i;
+        int temp = 0;
+        if (num == 0 | num == 1){
+            System.out.print(num + "Not a prime number");
         }
-        System.out.print(sum);
+        else{
+            for (int i=2; i<num; i++){
+                if(num%i == 0){
+                    temp ++;
+                    System.out.print(" "+i + "  ");
+                    break;
+                }
+            }
+            if (temp == 0)
+                System.out.print(num+" is a Prime number");
+            else
+                System.out.print(num + " Not a prime number");
+        }
+
     }
 }
