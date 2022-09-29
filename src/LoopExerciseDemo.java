b18bd31daad6c9ada278b2e7d5347b92f75d9a82
ruhulmus <ruhulmus@gmail.com>
2022-09-30 00:41:55 +0600

    loop Exercise demo

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -1,6 +1,6 @@
 public class LoopExerciseDemo {
     public static void main(String[] args) {
-        for (int i = 2; i<=100 ; i+=2){
+        for (int i = 100; i>=2 ; i-=2){
             System.out.print(i + " ");
         }
     }
