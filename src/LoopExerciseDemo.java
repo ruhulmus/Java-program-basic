1abf84360d53f24bf25c4fa04884076272590f88
ruhulmus <ruhulmus@gmail.com>
2022-10-01 00:12:04 +0600

    prime number check

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -6,27 +6,14 @@ public class LoopExerciseDemo {
         System.out.print("Please enter a number : ");
         int num = input.nextInt();
 
-        int count = 0;
+        boolean flag = true;
 
-        if (num == 0 | num ==1){
-            System.out.print(num+" Not a Prime number");
+        for (int i=2; num/2; i++){
+            if (num%i ==0 ){
+                flag = false;
+            }
+
         }
-        else{
-            for(int i=2; i<num;i++){
-                if(num%i == 0){
-                    count ++;
-                    break;
-                }
-
-            }
-            if(count == 0){
-                System.out.print(num + " Is Prime number");
-            }
-            else{
-                System.out.print(num+" Not a Prime number");
-            }
-        }
-
-
+//        System.out.print(flag ? "Prime number" : "Not a prime number");
     }
 }
