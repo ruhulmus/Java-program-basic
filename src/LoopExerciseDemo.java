02c2882cb28f71218c0d330b2b9e077504def9f5
ruhulmus <ruhulmus@gmail.com>
2022-10-01 11:55:04 +0600

    character add

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -4,18 +4,12 @@ public class LoopExerciseDemo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number : ");
-        int n = input.nextInt();
+        String n = input.nextLine();
 
-        int first = 1;
-        int second = 1;
-        int count;
-        System.out.print(first+" "+ second);
-        for (int i = 2 ; i < n; i++){
-            count = first + second;
-            first = second;
-            second = count;
-            System.out.print(" "+ count);
+        for (int i = 0; i <n.length() -1; i++){
+            System.out.print(n.charAt(i)+" ");
         }
 
     }
+
 }
