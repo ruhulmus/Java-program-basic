72b9a7ca19b067af7ba3a5a35fef447a82a083c4
ruhulmus <ruhulmus@gmail.com>
2022-10-02 00:40:57 +0600

    star pattern 2

---
+++ src/StarPattern2.java
@@ -0,0 +1,20 @@
+public class StarPattern2 {
+    public static void main(String[] args) {
+        for (int i=0; i<=5; i++){
+            for(int j=5; j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
+// output
+/*
+ ******
+ *****
+ ****
+ ***
+ **
+ *
+ */
