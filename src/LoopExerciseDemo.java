412fed0f1b74ddca299730324b08ce4c8759feb9
ruhulmus <ruhulmus@gmail.com>
2022-10-02 00:36:49 +0600

    start pattern 1

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -1,32 +1,11 @@
-import java.util.Scanner;
-
 public class LoopExerciseDemo {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your number : ");
-        String n = input.nextLine();
-        String value = n;
-        String reverse = "";
 
-        for (int i = n.length()-1; i >=0 ; i--){
-            reverse += n.charAt(i);
-//            System.out.print(reverse+" ");
+        for (int i=0; i<=5; i++){
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
-//        System.out.print(reverse);
-        if(n.equals(reverse))
-            System.out.print("Palindrom");
-        else
-            System.out.print("Not a palindrom");
-//        while(num !=0 ){
-//            int reminder = num%10;
-//            reverse = reverse * 10 + reminder;
-//            System.out.print( reverse+" ");
-//            num /=10;
-//        }
-//         if( n == reverse)
-//            System.out.print( "Palindrom");
-//        else
-//            System.out.print("Not a palindrom");
     }
-
 }
