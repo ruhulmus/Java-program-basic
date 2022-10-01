eaf5d0913e5ed3cfd5324345548189adf7ecf53a
ruhulmus <ruhulmus@gmail.com>
2022-10-01 13:20:58 +0600

    character reverse

--- src/LoopExerciseDemo.java
+++ src/LoopExerciseDemo.java
@@ -7,11 +7,11 @@ public class LoopExerciseDemo {
         String n = input.nextLine();
 
         String reverse = "";
-
-        for (int i = n.length()-1 ; i >=0 ; i--){
+        for(int i = n.length()-1 ; i >= 0; i--){
             reverse += n.charAt(i);
         }
         System.out.print(reverse);
+
     }
 
 }
