b203a105b5863d7f34037d77e5cd12a7950cd91a
ruhulmus <ruhulmus@gmail.com>
2022-10-02 01:41:56 +0600

    star pattern 4 piramid

--- src/StarPattern4.java
+++ src/StarPattern4.java
@@ -1,14 +1,14 @@
-// Left Triangle Star Pattern
-public class StarPattern3 {
+// Pyramid Star Pattern
+public class StarPattern4 {
     public static void main(String[] args) {
         int i,j,k, row = 5;
 
         for (i=1; i<=row; i++){
-            for(j=row-1; j>=i;j--){
+            for(j=1; j<=row-i;j++){
                 System.out.print(" ");
             }
             for(k=1; k<=i;k++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println("");
         }
