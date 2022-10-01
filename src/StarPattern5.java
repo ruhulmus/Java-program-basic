c9e10e606c960302f11d707c23bc8fef9844eede
ruhulmus <ruhulmus@gmail.com>
2022-10-02 01:57:53 +0600

    star pattern 4 pyramid 2

--- src/StarPattern5.java
+++ src/StarPattern5.java
@@ -1,14 +1,15 @@
-// Pyramid Star Pattern
-public class StarPattern4 {
+// Pyramid Star Pattern 2
+public class StarPattern5 {
     public static void main(String[] args) {
-        int i,j,k, row = 6;
+        int i,j,k, row = 5;
 
         for (i=1; i<=row; i++){
             for(j=1; j<=row-i;j++){
                 System.out.print(" ");
             }
-            for(k=1; k<=i;k++){
-                System.out.print("* ");
+            for(k=1; k<=(2*i)-1;k++){
+                System.out.print("*");
+
             }
             System.out.println("");
         }
