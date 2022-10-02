d6b7b0cc7c8ae4865815aefc16088b3569325ea8
ruhulmus <ruhulmus@gmail.com>
2022-10-02 21:30:53 +0600

    pyramid pattern

--- src/StarPattern6.java
+++ src/StarPattern6.java
@@ -3,18 +3,30 @@ public class StarPattern6 {
     public static void main(String[] args) {
         int i,j,k, row = 5;
 
-        for (i=1; i <=row; i++){
-            for(j=i; j<=row ;j++){
+//        for (i=1; i <=row; i++){
+//            for(j=i; j<=row ;j++){
+//                System.out.print(" ");
+//            }
+//            for (k=1; k<=i;k++){
+//                System.out.print("*");
+//            }
+//            for (int l=1; l<i;l++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        for (i=1;i<=row;i++){
+            for(j=i;j<=row-1;j++){
                 System.out.print(" ");
             }
-            for (k=1; k<=i;k++){
-                System.out.print("*");
-            }
-            for (int l=1; l<i;l++){
-                System.out.print("*");
+            for (k=1 ; k<=i; k++){
+                System.out.print("* ");
             }
             System.out.println();
         }
+
     }
 }
 // output
