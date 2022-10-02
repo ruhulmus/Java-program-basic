b3c45387ff1290e0d190d22dd5e1f6605e391f23
ruhulmus <ruhulmus@gmail.com>
2022-10-02 22:17:41 +0600

    diomond shape

--- src/StarPattern7.java
+++ src/StarPattern7.java
@@ -1,16 +1,25 @@
 // Diamond Shape Star Pattern
 public class StarPattern7 {
     public static void main(String[] args) {
-        int i,j,k, row = 8;
+        int i,j,k, row = 5;
 
         for (i=1;i<=row;i++){
+
             for(j=i;j<=row-1;j++){
                 System.out.print(" ");
             }
-            for (k=1 ; k<=i; k++){
+            for(k=1;k<=(i*2)-1;k++){
                 System.out.print("*");
             }
-            for (k=1 ; k<i; k++){
+            System.out.println();
+        }
+
+        for (i=row-1;i>=1;i--){
+
+            for(j=i;j<row;j++){
+                System.out.print(" ");
+            }
+            for(k=1;k<=(i*2)-1;k++){
                 System.out.print("*");
             }
             System.out.println();
