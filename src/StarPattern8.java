a310b4d3421a27c2da83ed5bc66f66f67dd0857c
ruhulmus <ruhulmus@gmail.com>
2022-10-02 21:56:30 +0600

    reverse pyramid pattern

--- src/StarPattern8.java
+++ src/StarPattern8.java
@@ -1,7 +1,7 @@
 // Reverse Pyramid Star Pattern
 public class StarPattern8 {
     public static void main(String[] args) {
-        int i,j,k, row = 8;
+        int i,j,k, row = 5;
 
         for (i=1;i<=row;i++){
             for(j=1;j<i;j++){
@@ -11,6 +11,18 @@ public class StarPattern8 {
                 System.out.print("* ");
             }
 
+            System.out.println();
+        }
+
+        System.out.println("=====================");
+
+        for(i=row; i>=1; i--){
+            for(j=i;j<=row-1;j++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=(i*2)-1; j++){
+                System.out.print("*");
+            }
             System.out.println();
         }
 
