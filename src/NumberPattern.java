1186782cb995f4d7a9722712ee369ed8e74017bd
ruhulmus <ruhulmus@gmail.com>
2022-10-10 19:18:13 +0600

    update

--- src/NumberPattern.java
+++ src/NumberPattern.java
@@ -41,7 +41,6 @@ public class NumberPattern {
             System.out.println();
         }
         System.out.println("====================");
-
         for(i=1;i<=row;i++){
             for(j=i;j<=row-1;j++){
                 System.out.print(" ");
