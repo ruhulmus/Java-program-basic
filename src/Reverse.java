1766bb721f58bcdf3bedee6ae85602bf17399b39
ruhulmus <ruhulmus@gmail.com>
2022-10-16 01:29:02 +0600

    reverse string

--- src/Reverse.java
+++ src/Reverse.java
@@ -7,8 +7,11 @@ public class Reverse {
 
         String str = input.nextLine();
 
+        String result = "";
         for (int i = str.length()-1 ; i >= 0 ; i-- ){
-            System.out.print(str.charAt(i)+" ");
+            result += str.charAt(i);
+
         }
+        System.out.print(result);
     }
 }
