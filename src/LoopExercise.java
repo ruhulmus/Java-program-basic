fe4408a4c7115f53c931394188c828b97a4bc7fd
ruhulmus <ruhulmus@gmail.com>
2022-10-15 20:37:04 +0600

    simple loop emxaple

--- src/LoopExercise.java
+++ src/LoopExercise.java
@@ -6,22 +6,24 @@ public class LoopExercise {
         System.out.println("Please Enter your number : ");
         int num = input.nextInt();
         int min = num ,max = num;
-        while(true){
-            num = input.nextInt();
-            if (num >=0){
-//                if (num >= max)
-//                    max = num;
-//                if (num <= min)
-//                    min = num;
+        if(num >=0){
+            while(true){
+                num = input.nextInt();
+                if (num < 0)
+                    break;
+    //                if (num >= max)
+    //                    max = num;
+    //                if (num <= min)
+    //                    min = num;
 
-                max = num >= max ? num : max;
-                min = num <= min ? num : min;
-            }
-            else{
-                System.out.println("max : "+ max + ", Min : "+ min);
-                break;
-            }
+                    max = num >= max ? num : max;
+                    min = num <= min ? num : min;
+                }
 
-        }
+                    System.out.println("max : "+ max + ", Min : "+ min);
+
+            }
+        else
+            System.out.println("invalid input");
     }
 }
