64abdf668bc9b0cb0648c2328f16bc1834e46f4c
ruhulmus <ruhulmus@gmail.com>
2022-10-16 01:27:58 +0600

    reverse string

---
+++ src/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your string : ");
+
+        String str = input.nextLine();
+
+        for (int i = str.length()-1 ; i >= 0 ; i-- ){
+            System.out.print(str.charAt(i)+" ");
+        }
+    }
+}
