872b7900897a5499d9a1904781a0e143661998b5
ruhulmus <ruhulmus@gmail.com>
2022-10-19 15:56:54 +0600

    start patter

--- src/StarPattern11.java
+++ src/StarPattern11.java
@@ -7,12 +7,29 @@ public class StarPattern11 {
         System.out.print("Please enter your number : ");
         int n = input.nextInt();
 
-        for (int i =1 ; i <=n;i++){
-            for (int j=1; j<=n;j++){
-                System.out.print("*");
+//        for (int i =1 ; i <=n;i++){
+//            for (int j=1; j<=n;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 1; i<=n;i++){
+            if(i==1 || i==n){
+                for(int j= 1 ; j<=n;j++){
+                    System.out.print("*");
+                }
+            }else {
+                for(int j= 1 ; j<=n;j++){
+
+                    if(j==1||j==n){
+                        System.out.print("*");
+                    }
+                    else
+                        System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
-
 }
