f8165090fab54ed576ac2306398fcee9e27600c6
ruhulmus <ruhulmus@gmail.com>
2022-10-19 13:39:53 +0600

    loop exersiz

--- src/Reverse.java
+++ src/Reverse.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Reverse {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your string : ");
-
+        System.out.print("Please enter your number : ");
+        int n = input.nextInt();
 //        String str = input.nextLine();
 //
 //        String result = "";
@@ -15,7 +15,15 @@ public class Reverse {
 //        System.out.print(result);
 
 
-
+//        for(int i=1;i<=n;i++){
+//            for(int j= n-1; j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for (int k = 1 ; k <=i;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
 
     }
 }
