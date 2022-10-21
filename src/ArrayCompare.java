2f2cf33db005b8e4c1a4aab642d40a1cecd50e55
ruhulmus <ruhulmus@gmail.com>
2022-10-21 23:36:01 +0600

    array compare

--- src/ArrayCompare.java
+++ src/ArrayCompare.java
@@ -6,6 +6,9 @@ public class ArrayCompare {
         int[] numers2 = {2,3,4,5,1,6,4,7};
         int[] numers3 = {2,9,4,5,1,6,4,7};
 
-        System.out.print(Arrays.equals(numers1,numers2));
+        String [] str1 = {"test","test1","test3"};
+        String [] str2 = {"test","test1","test3"};
+        System.out.print(str1 == str2);
+        System.out.print(Arrays.equals(str1,str2));
     }
 }
