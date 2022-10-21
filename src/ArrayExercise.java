ae0426e713cd40b3e24bb1e5a0ebed44494bd955
ruhulmus <ruhulmus@gmail.com>
2022-10-21 21:43:44 +0600

    array exercise

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -3,6 +3,12 @@ public class ArrayExercise {
         System.out.print("hellow wordl");
 
          int[] numbers = {2,3,4};
+         change(numbers);
+         printArray(numbers);
+    }
+    public static void change(int[] numers){
+        numers[0]=1;
+        numers[1]=2;
     }
 }
 
