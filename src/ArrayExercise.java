0186749e43719a53bfac7c548ca5f1d1340d9464
ruhulmus <ruhulmus@gmail.com>
2022-10-21 22:01:10 +0600

    array exercise

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -1,18 +1,10 @@
+import java.util.Arrays;
+
 public class ArrayExercise {
     public static void main(String[] args) {
-
-         int[] numbers = {2,3,4};
-         change(numbers);
-         printArray(numbers);
-    }
-    public static void change(int[] numers){
-        numers[0]=1;
-        numers[1]=5;
-    }
-    public static void printArray(int[] numers){
-        for(int i =0; i<numers.length;i++){
-            System.out.print(numers[i]+" ");
-        }
+        int[] numbers = {5,3,4,1,4,2,0};
+        Arrays.sort(numbers,2,7);
+        System.out.print(Arrays.toString(numbers));
     }
 }
 
