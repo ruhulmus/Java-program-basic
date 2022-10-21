fabc182fa101ae50aa2dda9310c3ae9580feaf72
ruhulmus <ruhulmus@gmail.com>
2022-10-21 21:42:19 +0600

    array exercise

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -2,12 +2,7 @@ public class ArrayExercise {
     public static void main(String[] args) {
         System.out.print("hellow wordl");
 
-
-        String [] car;
-        car = new String[10];
-
-
-        int[] number = new int[10];
+         int[] numbers = {2,3,4};
     }
 }
 
