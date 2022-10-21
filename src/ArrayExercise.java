bac782b2cca0d9f127977dd6db1715827a58e95a
ruhulmus <ruhulmus@gmail.com>
2022-10-21 22:29:30 +0600

    array exercise

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class ArrayExercise {
     public static void main(String[] args) {
         int[] numbers = {5,3,4,1,4,2,0};
-        Arrays.sort(numbers,2,7);
+        Arrays.sort(numbers);
+//        Arrays.sort(numbers,2,7);
         System.out.print(Arrays.toString(numbers));
+        System.out.print(Arrays.binarySearch(numbers,-8));
     }
 }
 
