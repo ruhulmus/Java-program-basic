7b22c53a4f3c0a05c37599c5b057decdaec74dff
ruhulmus <ruhulmus@gmail.com>
2022-10-22 01:16:13 +0600

    array compare

--- src/ArrayCompare.java
+++ src/ArrayCompare.java
@@ -10,5 +10,14 @@ public class ArrayCompare {
         String [] str2 = {"test","test1","test3"};
         System.out.print(str1 == str2);
         System.out.print(Arrays.equals(str1,str2));
+
+        System.out.println(sum(2,5,6,7));
+    }
+    public static int sum (int... numbers){
+        int sum =0;
+        for(int i =0; i<numbers.length;i++){
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
