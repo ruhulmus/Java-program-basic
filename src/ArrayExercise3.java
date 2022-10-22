38b64328ce5e21feb3bc6bb967ca4c8fa1c94f9a
ruhulmus <ruhulmus@gmail.com>
2022-10-22 22:23:50 +0600

    Array exercise

--- src/ArrayExercise3.java
+++ src/ArrayExercise3.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 public class ArrayExercise3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter your number : ");
-        int num = input.nextInt();
+//        System.out.print("Please enter your number : ");
+//        int num = input.nextInt();
+
+        // Please enter your number : 3
+        // 3 Occurs 1 times
+
         int[] numbers = {1,1,1,2,3,4};
 
-        int count = 0;
-        for(int i = 0; i<numbers.length;i++){
+//        int count = 0;
+//        for(int i = 0; i<numbers.length;i++){
+//
+//            if(numbers[i]==num){
+//                count ++;
+//            }
+//        }
+//        System.out.print(num + " Occurs "+ count + " times");
 
-            if(numbers[i]==num){
-                count ++;
 
-            }
+        //=====================
+
+        int min = 0;
+        int max = 0;
+
+        for(int i=0; i < numbers.length; i++) {
 
         }
-        System.out.print(num + " Occurs "+ count + " times");
     }
 }
