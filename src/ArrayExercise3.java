a3368b37ed583c860e6e4fca1ca339b9300ace90
ruhulmus <ruhulmus@gmail.com>
2022-10-22 22:28:44 +0600

    Array exercise

--- src/ArrayExercise3.java
+++ src/ArrayExercise3.java
@@ -9,7 +9,7 @@ public class ArrayExercise3 {
         // Please enter your number : 3
         // 3 Occurs 1 times
 
-        int[] numbers = {1,1,1,2,3,4};
+        int[] numbers = {1,1,1,2,-3,4};
 
 //        int count = 0;
 //        for(int i = 0; i<numbers.length;i++){
@@ -23,11 +23,15 @@ public class ArrayExercise3 {
 
         //=====================
 
-        int min = 0;
-        int max = 0;
+        int min = numbers[0];
+        int max = numbers[0];
 
         for(int i=0; i < numbers.length; i++) {
-
+            if (max <= numbers[i])
+                max = numbers[i];
+            if(min >= numbers[i])
+                min = numbers[i];
         }
+        System.out.print("Max : " + max + " , Min "+min);
     }
 }
