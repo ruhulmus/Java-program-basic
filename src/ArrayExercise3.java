1f9b48ff05b3ac7363e00ed9160c48c46bc2f7b5
ruhulmus <ruhulmus@gmail.com>
2022-10-22 22:30:19 +0600

    Array exercise

--- src/ArrayExercise3.java
+++ src/ArrayExercise3.java
@@ -27,10 +27,8 @@ public class ArrayExercise3 {
         int max = numbers[0];
 
         for(int i=0; i < numbers.length; i++) {
-            if (max <= numbers[i])
-                max = numbers[i];
-            if(min >= numbers[i])
-                min = numbers[i];
+            max = (max < numbers[i]) ? numbers[i] : max;
+            min = (min > numbers[i]) ? numbers[i] : min;
         }
         System.out.print("Max : " + max + " , Min "+min);
     }
