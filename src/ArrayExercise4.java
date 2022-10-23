f2cd39b8e633a88adee518749ab2ec54e288e1ec
ruhulmus <ruhulmus@gmail.com>
2022-10-23 22:19:38 +0600

    Array 2 dimensional

---
+++ src/ArrayExercise4.java
@@ -0,0 +1,13 @@
+public class ArrayExercise4 {
+    public static void main(String[] args) {
+        int[][] integers = {{3,5,7},{10,2,9}};
+
+
+        for(int i = 0; i<integers.length;i++){
+            System.out.print(i);
+
+            for(int j =0; j<)
+        }
+    }
+
+}
