e3e66577739cca349d4e3e32e5b7196480c97dd5
ruhulmus <ruhulmus@gmail.com>
2022-10-23 22:28:14 +0600

    Array 2 dimensional

--- src/ArrayExercise4.java
+++ src/ArrayExercise4.java
@@ -4,9 +4,20 @@ public class ArrayExercise4 {
 
 
         for(int i = 0; i<integers.length;i++){
-            System.out.print(i);
+//            System.out.println(integers[0][0]);
+//            System.out.println(integers[0][1]);
+//            System.out.println(integers[0][2]);
+//
+//
+//            System.out.println(integers[1][0]);
+//            System.out.println(integers[1][1]);
+//            System.out.println(integers[1][2]);
+//            System.out.println(integers[i].length);
 
-            for(int j =0; j<)
+            for(int j =0; j<integers[i].length;j++){
+                System.out.print(integers[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
