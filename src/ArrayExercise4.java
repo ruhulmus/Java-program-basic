0053829f824b9daea10f15b3570b8d9f3c16cc62
ruhulmus <ruhulmus@gmail.com>
2022-10-24 01:18:26 +0600

    Array 2 dimensional

--- src/ArrayExercise4.java
+++ src/ArrayExercise4.java
@@ -2,10 +2,16 @@ import java.util.Arrays;
 
 public class ArrayExercise4 {
     public static void main(String[] args) {
-        int[][] integers = {{3,5,7},{10,2,9}};
+        int[][] integers = {
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}
+        };
 
+        int rowSum = 0;
+        int colSum = 0;
 
-        for(int i = 0; i<3;i++){
+        for(int i = 0; i<integers.length;i++){
 //            System.out.println(integers[0][0]);
 //            System.out.println(integers[0][1]);
 //            System.out.println(integers[0][2]);
@@ -15,9 +21,11 @@ public class ArrayExercise4 {
 //            System.out.println(integers[1][1]);
 //            System.out.println(integers[1][2]);
 //            System.out.println(integers[i].length);
+//            System.out.println(integers[i]);
+//            rowSum += integers[i];
 
-            for(int j =0; j<2;j++){
-                System.out.print(integers[j][i] + " ");
+            for(int j =0; j<integers.length;j++){
+                System.out.print(integers[i][j] + " ");
             }
             System.out.println();
 
