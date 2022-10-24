0757ca6bdbca396ddefa4aa5d194fa196573e600
ruhulmus <ruhulmus@gmail.com>
2022-10-24 22:54:00 +0600

    array list exercise

--- src/ArrayListExercise.java
+++ src/ArrayListExercise.java
@@ -1,88 +1,85 @@
-import java.lang.invoke.CallSite;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class ArrayListExercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        printOptions();
-
-
+        displayOptions();
         ArrayList<Integer> numbers = new ArrayList<>();
-        int num;
 
-        while (true){
-            takeInput();
-            int option = input.nextInt();
-            switch (option){
-                case 1:
-                    System.out.print("Enter your Value : ");
-                    num = input.nextInt();
-                    addItem(numbers,num);
-                    System.out.println();
-                    takeInput();
-                    break;
-                case 2:
-                    System.out.println("Enter your Value : ");
+        while(true){
 
-                    num = input.nextInt();
-                    removeItem(numbers,num);
-                    takeInput();
-                    break;
-                case 3:
-                    displayItem(numbers);
-                    takeInput();
-                    break;
-                case 4:
-                    System.out.print("Bye !!");
-                    break;
-                default:
-                    System.out.println("Invalid input !!");
-                    takeInput();
+            int options = input.nextInt();
+            if(options == 1){
+                System.out.print("Enter your value for Add : ");
+                int item = input.nextInt();
+                addItem(numbers, item);
+                displayOptions();
             }
+            if(options == 2){
+                System.out.print("Enter your value for remove : ");
+                int item = input.nextInt();
+                removeItem(numbers,item);
+                displayOptions();
+            }
+            if(options == 3){
+                displayItems(numbers);
+                displayOptions();
+            }
+            if(options == 4){
+                System.out.println("Bye !");
+                break;
+            }
+//            switch (item){
+//                case 1:
+//                    System.out.print("Enter your value for Add : ");
+//                    addItem(numbers, item);
+//                    displayOptions();
+//                case 2:
+//                    System.out.print("Enter your value for remove : ");
+//                    removeItem(numbers,item);
+//                    displayOptions();
+//                case 3:
+//                    displayItems(numbers,item);
+//                    displayOptions();
+//                case 4:
+//                    System.out.println("Bye !");
+//                    break;
+//                default:
+//                    System.out.println("invalid input");
+//            }
         }
+    }
 
-
-    }
-    public static void addItem(ArrayList<Integer> numbers, int item){
-        numbers.add(item);
-        System.out.println(item + " Aded !!");
-    }
-    public static void removeItem(ArrayList<Integer> numbers, int item){
-        if(numbers.contains(item)){
-            numbers.remove(item);
-            System.out.println(item + " removed !");
-        }
-        else{
-            System.out.println(item + " not found !");
-            System.out.println();
-        }
-    }
-    public static void displayItem(ArrayList<Integer> numbers){
+    private static void displayItems(ArrayList<Integer> numbers) {
         Collections.sort(numbers);
         System.out.println(numbers);
     }
 
-    private static void takeInput() {
-        Scanner input = new Scanner(System.in);
+    private static void removeItem(ArrayList<Integer> numbers, int item) {
+        if(numbers.contains(item)){
+            numbers.remove(item);
+            System.out.println("Remove !");
+
+        }
+        else{
+            System.out.println("Not Found");
+        }
+
+    }
+
+    private static void addItem(ArrayList<Integer> numbers, int item) {
+        numbers.add(item);
+        System.out.println("Added !");
+    }
+
+    private static void displayOptions() {
+        System.out.println();
         System.out.println("1. Add");
         System.out.println("2. Remove");
         System.out.println("3. Display");
         System.out.println("4. Exit");
-        System.out.println();
-        System.out.print("Your Choice : ");
-        int num = input.nextInt();
+        System.out.print("Enter your Choice : ");
     }
-
-    private static void printOptions() {
-        System.out.println("1. Add");
-        System.out.println("2. Remove");
-        System.out.println("3. Display");
-        System.out.println("4. Exit");
-        System.out.println();
-        System.out.print("Your Choice : ");
-    }
-
 }
