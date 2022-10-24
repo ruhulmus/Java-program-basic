4d61fc5ecad25bb4cb413f50af67ea77cfd24b58
ruhulmus <ruhulmus@gmail.com>
2022-10-24 15:14:26 +0600

    arraylist exercis

--- src/ArrayList1.java
+++ src/ArrayList1.java
@@ -2,10 +2,15 @@ import java.util.ArrayList;
 
 public class ArrayList1 {
     public static void main(String[] args){
-        ArrayList<Integer> integers = new ArrayList<>();
-        integers.add(1);
-        integers.add(5);
-        integers.add(2,4);
-        System.out.println(integers);
+//        ArrayList<Integer> integers = new ArrayList<>();
+//        integers.add(1);
+//        integers.add(5);
+//        integers.add(2,4);
+//        for(Integer num : integers){
+//            System.out.print(num+" ");
+//        }
+//        System.out.println(integers);
+
+
     }
 }
