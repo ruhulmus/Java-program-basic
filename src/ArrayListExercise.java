f956b6dfa57b8ca6d60363a1f9ad94bcf4190725
ruhulmus <ruhulmus@gmail.com>
2022-10-24 22:24:50 +0600

    array list exercise

--- src/ArrayListExercise.java
+++ src/ArrayListExercise.java
@@ -1,5 +1,6 @@
 import java.lang.invoke.CallSite;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -7,42 +8,61 @@ public class ArrayListExercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         printOptions();
-        int option = input.nextInt();
+
 
         ArrayList<Integer> numbers = new ArrayList<>();
         int num;
-        switch (option){
-            case 1:
-                System.out.print("Enter your Value : ");
-                num = input.nextInt();
-                numbers.add(num);
-                System.out.println(num + " Aded !!");
-                takeInput();
-                break;
-            case 2:
-                System.out.println("Enter your Value : ");
-                num = input.nextInt();
-                if(numbers.contains(num)){
-                    numbers.remove(num);
-                    System.out.println(num + " removed !");
-                }
-                else{
-                    System.out.println(num + " not found !");
-                }
-                takeInput();
-                break;
-            case 3:
-                Collections.sort(numbers);
-                System.out.println(numbers);
-                takeInput();
-                break;
-            case 4:
-                System.out.print("Bye !!");
-                break;
-            default:
-                System.out.println("Invalid input !!");
-                takeInput();
+
+        while (true){
+            takeInput();
+            int option = input.nextInt();
+            switch (option){
+                case 1:
+                    System.out.print("Enter your Value : ");
+                    num = input.nextInt();
+                    addItem(numbers,num);
+                    System.out.println();
+                    takeInput();
+                    break;
+                case 2:
+                    System.out.println("Enter your Value : ");
+
+                    num = input.nextInt();
+                    removeItem(numbers,num);
+                    takeInput();
+                    break;
+                case 3:
+                    displayItem(numbers);
+                    takeInput();
+                    break;
+                case 4:
+                    System.out.print("Bye !!");
+                    break;
+                default:
+                    System.out.println("Invalid input !!");
+                    takeInput();
+            }
         }
+
+
+    }
+    public static void addItem(ArrayList<Integer> numbers, int item){
+        numbers.add(item);
+        System.out.println(item + " Aded !!");
+    }
+    public static void removeItem(ArrayList<Integer> numbers, int item){
+        if(numbers.contains(item)){
+            numbers.remove(item);
+            System.out.println(item + " removed !");
+        }
+        else{
+            System.out.println(item + " not found !");
+            System.out.println();
+        }
+    }
+    public static void displayItem(ArrayList<Integer> numbers){
+        Collections.sort(numbers);
+        System.out.println(numbers);
     }
 
     private static void takeInput() {
