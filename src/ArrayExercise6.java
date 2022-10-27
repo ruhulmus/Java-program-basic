12655c3e0c63610c5511dd5ce97e37b0a729d74e
ruhulmus <ruhulmus@gmail.com>
2022-10-28 03:17:50 +0600

    position wise array value find

--- src/ArrayExercise6.java
+++ src/ArrayExercise6.java
@@ -1,2 +1,20 @@
 public class ArrayExercise6 {
+    public static void main(String[] args) {
+        int arr[] = { 12, 34, 10, 6, 40 };
+        int position = 5;
+        System.out.println(getValue(arr,position));
+    }
+
+    private static int getValue(int[] arr, int position) {
+         for(int i=0;i<arr.length;i++){
+            if(arr[i]==arr[position-1]){
+                return arr[i];
+            }
+        }
+
+
+
+        return -1;
+    }
+
 }
