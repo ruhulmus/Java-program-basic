492521ae6ef067772ed59e809d93f85144c4c3fb
ruhulmus <ruhulmus@gmail.com>
2022-10-28 03:20:42 +0600

    position wise array value find

--- src/ArrayExercise6.java
+++ src/ArrayExercise6.java
@@ -2,18 +2,21 @@ public class ArrayExercise6 {
     public static void main(String[] args) {
         int arr[] = { 12, 34, 10, 6, 40 };
         int position = 5;
-        System.out.println(getValue(arr,position));
+        int value = 10;
+        System.out.println();
+        int val = findValue(arr,position);
+        if(val==-1)
+            System.out.println("not found !!");
+        else
+            System.out.println(val);
     }
 
-    private static int getValue(int[] arr, int position) {
+    private static int findValue(int[] arr, int position) {
          for(int i=0;i<arr.length;i++){
             if(arr[i]==arr[position-1]){
                 return arr[i];
             }
         }
-
-
-
         return -1;
     }
 
