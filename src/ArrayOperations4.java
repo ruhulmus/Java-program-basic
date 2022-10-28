56828715c9e87f60cd29b1e7761e40fc60b71291
ruhulmus <ruhulmus@gmail.com>
2022-10-29 02:09:44 +0600

    insert new valuer into an unsorted array

---
+++ src/ArrayOperations4.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class ArrayOperations4 {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 8, 5};
+        int position = 3;
+        System.out.println("Old Array : "+ Arrays.toString(arr));
+//        for(int i = 2 ; i < arr.length-1;i++){
+//            arr[i-1]=arr[i];
+//        }
+        for(int i = arr.length-1; i>position-1;i--){
+            arr[i-1]=arr[i];
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int[] nums = {3,2,4};
+        int target = 6;
+
+        int[] arr2 = new int[2];
+        
+        int[] test= getSum(nums, target);
+        if(test ==null ){
+            System.out.println("not found");
+        }else
+             System.out.println(Arrays.toString(test));
+
+    }
+
+    private static int[] getSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+
+            for(int j=i+1; j<nums.length;j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+}
