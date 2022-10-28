48e0e6754863f698344c26a72749edb574af5f5d
ruhulmus <ruhulmus@gmail.com>
2022-10-28 23:03:32 +0600

    insert new valuer into an unsorted array

---
+++ src/ArrayOperations3.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ArrayOperations3 {
+    // Given array =v   [2, 4, 1, 8, 5] ;
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+        arr[0]=2;
+        arr[1]=4;
+        arr[2]=1;
+        arr[3]=8;
+        arr[4]=5;
+
+       int position  = 3;
+       int index = position-1;
+       int value = 10;
+        System.out.println("actual Array : " + Arrays.toString(arr));
+
+        for(int i = arr.length -1; i>index; i--){
+                arr[i] = arr[i-1];
+          System.out.println("asd "+ arr[i-1]);
+        }
+        arr[index]=value;
+        System.out.print(Arrays.toString(arr));
+    }
+
+
+}
