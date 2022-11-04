12ea4dc7364f14deaa6c9f1da810406b268f57f8
ruhulmus <ruhulmus@gmail.com>
2022-11-04 23:50:15 +0600

    longest substring

--- src/ValidPalindrop_125.java
+++ src/ValidPalindrop_125.java
@@ -8,13 +8,12 @@ public class ValidPalindrop_125 {
         String result = "";
         for(int i = str.length()-1; i>=0; i--){
             result += str.charAt(i);
-
         }
         System.out.println(result+" aaa");
         if(result.equals(str)){
             System.out.println("Palindrom");
         }else
             System.out.println("Not a palindrom");
-
     }
+
 }
